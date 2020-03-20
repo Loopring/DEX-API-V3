01a9de04e4ce8c5bbb5d82f5d2ff59a4e8c7216f
Hoss <hosschao@gmail.com>
2020-03-20 15:28:27 +0800

    publish 20200320-152819

--- publish.sh
+++ publish.sh
@@ -3,4 +3,6 @@
 # Copyright 2020 Loopring Org. All Rights Reserved.
 # Author: chao@loopring.org (Ma Chao)
 
-./xdoc.py build && cd generated && gitbook build . docs && cd .. && cp -rf generated/docs .
+time=$(date "+%Y%m%d-%H%M%S")
+
+./xdoc.py build && cd generated && gitbook build . docs && cd .. && cp -rf generated/docs . && git add . && git commit -m "publish ${time}" && git push origin master
