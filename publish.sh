d8b072eaa04206e6b165d784061c94980d41fc9d
Hoss <hosschao@gmail.com>
2020-03-23 13:19:48 +0800

    publish 20200323-131938

--- publish.sh
+++ publish.sh
@@ -5,4 +5,4 @@
 
 time=$(date "+%Y%m%d-%H%M%S")
 
-./xdoc.py build && cd generated && gitbook build . docs && cd .. && cp -rf generated/docs . && git add . && git commit -m "publish ${time}" && git push origin master
+./xdoc.py build && cd generated && gitbook build . docs && cd .. && rm -rf ./docs && cp -rf generated/docs . && git add . && git commit -m "publish ${time}" && git push origin master
