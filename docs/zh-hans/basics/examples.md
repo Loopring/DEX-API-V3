e456f78334c90004737a51dc24927b3ff2702423
yue.wang <wangyue@loopring.org>
2020-12-18 01:29:55 +0800

    publish 20201218-012945

---
+++ docs/zh-hans/basics/examples.md
@@ -0,0 +1,9 @@
+# 示范代码
+
+**[https://github.com/Loopring/hello_loopring](https://github.com/Loopring/hello_loopring)**代码仓库包含了一些简单示例，帮助您快速上手使用路印API。其中：
+
+- **hash_and_sign/**：使用Python和ethsnarks开源仓库，计算任意字符串的Poseidon哈希，并对其做EdDSA的签名和验签。
+- **trading/**：提交订单，读取订单，和取消订单。
+- **offchain_withdrawal/**：链下提现的签名和提交（未完成）。
+
+我们后续会不断完善，开源更多的代码教程。敬请期待。
--- publish.sh
+++ publish.sh
@@ -8,10 +8,10 @@ time=$(date "+%Y%m%d-%H%M%S")
 cd generated && \
     rm -rf $(ls | grep -v "node_modules") && \
     cd .. && \
-    ./xdoc.py build && \
+    python ./xdoc.py build && \
     cd generated && \
     gitbook build . docs && \
     cd .. && rm -rf ./docs && \
     cp -rf generated/docs . && \
-    ./xdoc.py windup && \
-    git add . && git commit -m "publish ${time}" && git push v3 master
+    python ./xdoc.py windup && \
+    git add . && git commit -m "publish ${time}" && git push v3 debug-v3
