e1ad9bca7d3882a4e63133135a23e3f011dbe6d0
kongliang <kongliangzhong@gmail.com>
2021-10-28 18:04:34 +0800

    publish 20211028-180418

--- publish.sh
+++ publish.sh
@@ -8,10 +8,10 @@ time=$(date "+%Y%m%d-%H%M%S")
 cd generated && \
     rm -rf $(ls | grep -v "node_modules") && \
     cd .. && \
-    python ./xdoc.py build && \
+    python3 ./xdoc.py build && \
     cd generated && \
     gitbook build . docs && \
     cd .. && rm -rf ./docs && \
     cp -rf generated/docs . && \
-    python ./xdoc.py windup && \
+    python3 ./xdoc.py windup && \
     git add . && git commit -m "publish ${time}" && git push origin v3-draft
